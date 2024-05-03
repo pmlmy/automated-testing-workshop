7f43f353eda47bc8d05a9eea122e533cc07bc27d
Dorell James <galangdj@gmail.com>
2024-05-03 01:56:51 +0000

    updated code function

--- person.js
+++ person.js
@@ -1,5 +1,19 @@
+// person.js
 function getFullName(participant) {
+	if (!participant?.firstName && !participant?.lastName) {
+		return "First name and last name is required!"
+	}
+	
+	if (!participant.lastName) {
+		return "Last name is required!"
+	}
+
+	if (!participant.firstName) {
+		return "First name is required!"
+	}
+
   return participant.firstName + " " + participant.lastName;
 }
+
 
 module.exports = { getFullName };

df77ab7c9d1e373ee03ecc585cb8688f344d52ac
Dorell James <galangdj@gmail.com>
2024-07-26 15:19:35 +0000

    chore: setup 100% coverage

--- person.test.js
+++ person.test.js
@@ -6,3 +6,33 @@ test('getFullName without first name and last name is handled properly', () => {
     const result = getFullName(participant);
     expect(result).toBe(expected);
 });
+
+test('getFullname without lastName but firstName is present returns "Last name is required"', () => {
+    const participant = {firstName: "Dorell"}
+
+    const expected = "Last name is required!"
+
+    const result = getFullName(participant)
+
+    expect(result).toBe(expected)
+})
+
+test('getFullname without firstName but lastName is present returns "First name is required"', () => {
+    const participant = {lastName: "Dorell"}
+
+    const expected = "First name is required!"
+
+    const result = getFullName(participant)
+
+    expect(result).toBe(expected)
+})
+
+test("getFullname with proper data returns firstname lastname", () => {
+    const participant = {firstName: "Dorell", lastName: "Gwapo"}
+
+    const expected = "Dorell Gwapo"
+
+    const result = getFullName(participant)
+
+    expect(result).toBe(expected)
+})

c9805f1d3c66a7e93910544ffe3e0a478e006110
Dorell James <galangdj@gmail.com>
2024-07-26 15:45:09 +0000

    initial setu

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,7 @@
 import React from "react";
 import "./App.css";
-
-function Greeting({ person }) {
-  return <p>Hello, {person.name}</p>;
-}
-
-function Introduction({ person }) {
-  return (
-    <div className="card">
-      <Greeting person={person} />
-      <p>I am {person.age} years old.</p>
-    </div>
-  );
-}
-
-const person = {
-  name: "John",
-  age: 25,
-};
+import Router from "./Router";
 
 export default function App() {
-  return <Introduction person={person} />;
+  return <Router />
 }
---
+++ src/Router.jsx
@@ -0,0 +1,24 @@
+// Router.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './routes/Home';
+import About from './routes/About';
+
+const RouterApp = () => {
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default RouterApp;

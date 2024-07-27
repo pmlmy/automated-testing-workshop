c897ac50f99f8847f3df90db34e9e8ee5acaa97d
Pamela May Tanedo <pamela.tanedo@rafi.ph>
2024-07-27 14:56:12 +0800

    init

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
+export default [
+  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+
+
+  {
+	  "rules": {
+	    semi: [2, "always"],
+	    quotes: [2, "double"],
+	  }
+	}
+  
+];
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-var participant = {firstName:'John',lastName:"Doe",age:25, attending:true}
+var participant = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 25,
+  attending: true,
+};
+
+console.log(participant);
